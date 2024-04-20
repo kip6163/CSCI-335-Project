4fda2ba3e40821b0f763ca9cd3991fa6bb516606
Alexander Lemieux <xancamo@gmail.com>
2024-04-20 15:25:48 -0400

    Added TimeHist.py and the needed code to save the time each epoch took to run during training and plot those times at the end of training

---
+++ TimeHist.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+from tensorflow import keras
+import time
+# from https://stackoverflow.com/a/43186440
+class TimeHistory(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.times = []
+
+    def on_epoch_begin(self, batch, logs={}):
+        self.epoch_time_start = time.time()
+
+    def on_epoch_end(self, batch, logs={}):
+        self.times.append(time.time() - self.epoch_time_start)
+
+
